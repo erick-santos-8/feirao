58d2e1f43f8b5f6a508801c0b00176ca524718f7
erick-santos-8 <esagosto@hotmail.com>
2024-08-26 16:39:34 -0300

    connectDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,8 @@ export const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log("Error: ",error.message)
+        process.exit(1);
+        //1 é erro e 0 sucesso  
     }
     
 }
