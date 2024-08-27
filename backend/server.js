accc0833039c20fb0135b1947a2eba8ceb98a847
erick-santos-8 <esagosto@hotmail.com>
2024-08-27 14:50:15 -0300

    backend basics

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); //Permite aceitar dados em JSON no req.body
 
-app.get("/products", (req, res) =>{
-    // res.send("Servidor funcionando!")
-})
 
-app.listen(5000, ()=>{
+app.use("/api/products", productRoutes);
+
+
+app.listen(PORT, ()=>{
     connectDB();
-    console.log("Servidor rodando na porta 5000.")           
+    console.log("Servidor rodando na porta "+PORT)           
 })
