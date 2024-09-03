d25415710df172b3679e94ece1d79a9c08b12a5e
erick-santos-8 <esagosto@hotmail.com>
2024-09-03 16:50:34 -0300

    scripts

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import path from "path";
+
 
 import productRoutes from "./routes/product.route.js";
 
@@ -11,9 +13,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); //Permite aceitar dados em JSON no req.body
 
+const __dirname = path.resolve();
+
 
 app.use("/api/products", productRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, ()=>{
     connectDB();
